b3047aedc3e2622a8b86e4309bd2b21301a6fb5d
Joe McIlvain <joe.eli.mac@gmail.com>
2013-08-05 23:14:21 -0400

    Cleaning of initial file

--- lib/wires/test.rb
+++ lib/wires/test.rb
@@ -11,34 +11,4 @@ module Wires
     Turn.config.trace   = 5
   end
   
-  # module TestModule
-    
-  #   def before_setup
-  #     @received_wires_events = []
-  #     on :event do |e|
-  #       @received_wires_events << e
-  #     end
-      
-  #     Hub.run
-  #     super
-  #   end
-    
-  #   def after_teardown
-  #     super
-  #     Hub.kill
-      
-  #     @received_wires_events.clear
-  #   end
-    
-  #   def assert_fired event
-  #     e = Event.new_from event
-  #     assert (@received_wires_events.include? e),
-  #       "Expected #{event.inspect} event to have been fired."
-  #   end
-    
-  # end
-  
-  # class Test < Minitest::Test;  include TestModule;  end
-  # class Spec < Minitest::Spec;  include TestModule;  end
-  
 end
